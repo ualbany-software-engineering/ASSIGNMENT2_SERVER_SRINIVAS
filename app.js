370b7470d5194af421a02cc910d2a546812e6f8b
Srinivas <sganta@albany.edu>
2022-10-09 19:49:57 -0400

    EXPRESS JS BACKEND CODE

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors())
+
+app.use('/add-numbers',(req, res) => {
+
+    const { firstParam, secondParam } = req.query;
+    const sum = Number(firstParam) + Number(secondParam);
+
+    res.json({ firstParam, secondParam, sum })
+});
+
+app.use('/', (req,res) => {
+    res.status(404).json({ message: 'Not Found'})
+})
+
+app.listen(8080)
